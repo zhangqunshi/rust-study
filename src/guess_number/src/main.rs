f1d5220eeadf618742879e2c04d3d66342e16f60
zhangqunshi <zhangqunshi@126.com>
2018-03-16 13:28:59 +0800

    finish guess number

--- src/guess_number/src/main.rs
+++ src/guess_number/src/main.rs
@@ -5,20 +5,29 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_rang(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please enter a number");
-    let mut guess = String::new();
+    loop {
+        println!("Please enter a number");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Fail to read line");
+        io::stdin().read_line(&mut guess).expect("Fail to read line");
 
-    println!("you enter number is {}", guess); 
+        println!("you enter number is {}", guess); 
 
-    let guess: u32 = guess.trim().parse().expect("plz type a number")
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::less => println!("Too small");
-        Ordering::Greater => println!("Too big");
-        Ordering::Equal => println!("You win");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            } 
+        }
     }
+    
 }
