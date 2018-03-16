a12beb9591ce6cd99e2e2029554861f575db1a76
zhangqunshi <zhangqunshi@126.com>
2018-03-16 12:51:48 +0800

    get user input

--- src/guess_number/src/main.rs
+++ src/guess_number/src/main.rs
@@ -1,3 +1,14 @@
+
+
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Please enter a number");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Fail to read line");
+
+    println!("you enter number is {}", guess); 
 }
